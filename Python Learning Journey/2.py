5ca0419ff4f3f1bbc5088d7670362462aa9c20c1
smaviaamir21 <70176109@student.uol.edu.pk>
2025-08-18 14:59:45 +0500

    Comments in  python

---
+++ Python Learning Journey/2.py
@@ -0,0 +1,14 @@
+print("Hello World")
+# print Hello World
+
+print(8,8)
+print("8+2")
+print(8+2)
+
+'''Hye, I am a good girl. I am learning python. This is my second
+ program. I am enjoying learning Python.'''
+
+print("Hello ",2,4,sep="---",end="----009\n")
+
+"""This is the way to write comments in program. 
+Comments in program is very useful for others to understand what is in program."""
