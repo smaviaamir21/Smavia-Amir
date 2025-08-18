4769aea875e9c986128ddd9cf146afcbe691cabd
smaviaamir21 <70176109@student.uol.edu.pk>
2025-08-18 16:15:40 +0500

    variables and data types

---
+++ Python Learning Journey/3.py
@@ -0,0 +1,25 @@
+# Variables and Data Types
+
+a=1264735
+print(a)
+smavia=4
+c=smavia  
+print(c)
+b="Smavia"
+print(b)
+
+a1=20
+b1=30
+print(a1+b1,a1-b1,a1*b1,a1%b1,a1/b1)
+
+# Find the data type of variable
+
+x=1
+y=True
+z="Smavia"
+u=None
+
+print("The type of x is ", type(x))
+print("The type of y is ", type(y))
+print("The type of z is ", type(z))
+print("The type of u is ", type(u))
