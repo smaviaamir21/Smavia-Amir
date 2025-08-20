1f289f85a21fcc909a4901fb9fd8c7947857ab09
smaviaamir21 <70176109@student.uol.edu.pk>
2025-08-20 17:32:13 +0500

    explicit typecasting

---
+++ Python Learning Journey/5.py
@@ -0,0 +1,7 @@
+#explicit typecasting
+string="15"
+number=7
+string_number=int(string)
+#throw an error if the string is not a valid integer
+sum=number+string_number
+print("The sum of both numbers is :", sum)
