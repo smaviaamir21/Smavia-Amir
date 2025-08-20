72503c5a72c20650c622a8ec469f824f5cad1964
smaviaamir21 <70176109@student.uol.edu.pk>
2025-08-20 17:31:05 +0500

    calculator using python

---
+++ Python Learning Journey/4.py
@@ -0,0 +1,7 @@
+a=5
+b=3
+print("a:",a,"b:",b)
+print("Adition between a and b is: ", a+b)
+print("Subtraction between a and b is: ", a-b)
+print("Muliplication between a and b is: ", a*b)
+print("Division between a and b is: ", a/b)
