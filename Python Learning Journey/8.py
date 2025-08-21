5d505f63709fd78eb4c18148d27738383c0fcb40
smaviaamir21 <70176109@student.uol.edu.pk>
2025-08-21 21:08:40 +0500

    strings in python

---
+++ Python Learning Journey/8.py
@@ -0,0 +1,18 @@
+#characters
+
+name="Smavia"
+friend="Noor"
+other_friend="Irza"
+print("My name is ", name,"and I have two friends", friend,"and ", other_friend)
+
+#string
+
+apple="I want to eat apple."
+print(apple)
+
+#string (two or more lines)
+
+lines="""I want to eat :
+Pizza
+because Pizza is my favourite."""
+print(lines)
