a20f7270541d990763df374f5efa4db34c10efbf
smaviaamir21 <70176109@student.uol.edu.pk>
2025-08-22 18:23:34 +0500

    index finding

---
+++ Python Learning Journey/9.py
@@ -0,0 +1,26 @@
+#indexes of characters in python
+
+name="Hassan"
+friend="Ahmad"
+other_friend="Mosa"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+
+# by using for loop
+
+for character in friend:
+    print(character)
+for character in other_friend:
+    print(character)
+
+# index of srting 
+string="""My name is Smavia.
+I love Coding.
+I am a good girl."""
+print(string)
+for character in string:
+    print(character)
