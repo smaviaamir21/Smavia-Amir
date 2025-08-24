f0d0b2227c1057c73bfd463272e36c3ce5794641
Smavia Amir <70176109@student.uol.edu.pk>
2025-08-24 17:42:26 +0500

    if else condition

---
+++ Python Learning Journey/12.py
@@ -0,0 +1,9 @@
+# Check if a number is even or odd
+
+print("=== Even or Odd Checker ===")
+number = int(input("Enter a number: "))
+
+if (number % 2 == 0):
+    print(number," is Even.")
+else:
+    print(number," is Odd.")
