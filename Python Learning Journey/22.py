4632247feaaf91b388c5243cb1c0437170b4c19a
Smavia Amir <70176109@student.uol.edu.pk>
2025-08-28 07:40:23 -0700

    Create 22.py

---
+++ Python Learning Journey/22.py
@@ -0,0 +1,22 @@
+#function arguments
+def name(fname,mname, lname):
+    print("Hello",fname,mname, lname)
+name("smavia", "Amir", "Ali")
+
+# required arguments
+def average(a,b):
+    print("The average is:",(a+b)/2)
+
+a=2
+b=3   
+average(a, b)
+
+# default arguments
+def average(a=3, b=9):
+    print("The average is:",(a+b)/2)
+average()
+
+# keyword arguments
+def average(a=3, b=9):
+    print("The average is:",(a+b)/2)
+average(b=15, a=9)
