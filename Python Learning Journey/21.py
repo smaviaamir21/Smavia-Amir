85c3c6e040d20e0f7907f7faf45c31a4c78b43fd
Smavia Amir <70176109@student.uol.edu.pk>
2025-08-28 07:12:40 -0700

    function in python

---
+++ Python Learning Journey/21.py
@@ -0,0 +1,10 @@
+#functions in python
+
+def calculater(a,b):
+    print("Addition:", a+b , "\nMuliplication:", a*b, "\nDivision:", a%b  )
+    
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+
+print("The caclulations between thses two number is:")
+calculater(a,b)
