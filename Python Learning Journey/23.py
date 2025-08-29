b3a36236dfa316e09f3755de6fdfcba7b8d5f037
Smavia Amir <70176109@student.uol.edu.pk>
2025-08-29 09:59:28 -0700

    list in python

---
+++ Python Learning Journey/23.py
@@ -0,0 +1,19 @@
+# list in python
+marks=[50,40, 79]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[1])
+print(marks[2])
+
+# list index
+color=["red","blue", "pink","yellow"]
+print(color[0])
+print(color[2])
+print(color[3])
+
+
+if "blue" in color:
+    print("yes")
+else:
+    print("no")
