bd09b92711483bd6bdea6429703c3bcd949cc60f
Smavia Amir <70176109@student.uol.edu.pk>
2025-09-08 08:57:49 -0700

    sets in  python

---
+++ Python Learning Journey/28.py
@@ -0,0 +1,13 @@
+# sets in python
+s={2,78,"smavia", 7.99, 8, 8}
+print(s)
+
+empty={}   # it will show class dict 
+print(type(empty))
+
+# To show the empty set the syntex is:
+empty=set()
+print(type(empty)) # it will show class set 
+
+for value in s:
+    print(value)
