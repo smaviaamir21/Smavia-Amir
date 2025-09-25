fcdcf4b95d53bc85a0ee07215ade1041fbd52af4
Smavia Amir <70176109@student.uol.edu.pk>
2025-09-25 02:08:42 -0700

    constructor in python

--- Python Learning Journey/46.py
+++ Python Learning Journey/46.py
@@ -1,1 +1,17 @@
-dfghr
+# constructor in python
+class person:
+    def __init__(self,name,age,level):
+        print("Hye i am a coder!")
+        self.name=name
+        self.age=age
+        self.level=level
+    def info(self):
+        print("{self.name} is {self.age} years old and in {self.level} class.")
+
+
+a=person('Smavia',12,8)
+b=person('Noor',10,7)
+c=person('Aqba',10,7)
+a.info()
+b.info()
+c.info()
