14ac0b7f11bdcc3023ca510238b6c49deed9d4a7
Smavia Amir <70176109@student.uol.edu.pk>
2025-09-26 10:50:24 -0700

    decorators in python

---
+++ Python Learning Journey/47.py
@@ -0,0 +1,16 @@
+# decorators in python
+def greet(fx):
+    def mfx(*args,**kwargs):
+        print("Good Morning!")
+        fx(*args,**kwargs)
+        print("Thanks for using this function.")
+    return mfx
+
+@greet
+def hello():
+    print("Hello World!")
+def add(a,b):
+    print(a+b)
+
+hello()
+greet(add)(2,3)   # another method 
