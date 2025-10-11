fd2f25fe00ed59a0bb3a49044ebc7e28ccb0c17d
Smavia Amir <70176109@student.uol.edu.pk>
2025-10-11 06:12:58 -0700

    os module

--- Python Learning Journey/54.py
+++ Python Learning Journey/54.py
@@ -1,1 +1,15 @@
+#import function
+import os
+#it will create data name folder
+# os.mkdir("data")
 
+if(not os.path.exists("data")):
+    os.mkdir("data")
+
+# create 100 day name files in data name folder
+for i in range(0,100):
+    os.mkdir(f"data/day{i+1}")
+
+#this will rename all 100 files
+# for i in range(0,100):
+#     os.rename(f"data/day{i+1}",f"data/lecture {i+1}")
