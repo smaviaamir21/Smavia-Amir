7a17c28cacfda4f46a2a0c9f86556323500f5187
Smavia Amir <70176109@student.uol.edu.pk>
2025-10-14 08:13:41 -0700

    clear and clutter files from a folder

--- Python Learning Journey/55.py
+++ Python Learning Journey/55.py
@@ -1,1 +1,12 @@
+# clear and clutter files from a folder
+import os
 
+files = os.listdir("data")
+i=1
+for file in files:
+    # print all files from folder
+    # print(file)
+    if file.endswith(".py"):  # select the files from these
+        print(file)   # print selected files
+    os.rename(f"data/{file}",f"data/{i}.py")   # rename and arrange all in a sequance
+    i=i+1
